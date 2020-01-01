9fc59744978e7dba5a2265bd84337e465112a75f
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-01-01 04:09:57 +0100

    Update copyright statements to 2020

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@
 ** More Ores **
 By Calinou, with the help of Nore.
 
-Copyright © 2011-2019 Hugo Locurcio and contributors.
+Copyright © 2011-2020 Hugo Locurcio and contributors.
 Licensed under the zlib license. See LICENSE.md for more information.
 =====================================================================
 --]]
